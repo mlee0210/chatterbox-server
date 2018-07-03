d27444a512ac15d152ab37e2a4e6323d1756fcff
mlee0210 <lee.cs.michael@gmail.com>
2018-07-02 17:33:09 -0700

    request and response

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,6 +12,8 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 
+
+
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
@@ -28,7 +30,6 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
   // The outgoing status.
   var statusCode = 200;
 
@@ -39,7 +40,11 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
+  
+
   headers['Content-Type'] = 'text/plain';
+  //headers['Content-Type'] = 'application/json';
+  
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -52,7 +57,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  response.end('Hello, Dudesssss!' + request);
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -71,3 +76,5 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+exports.requestHandler = requestHandler;
+// exports.requestHandler = requestHandler;  
